70621bedde53134d90ff4bfb1b2e08e9104bd97d
Avaaren <avaaren@mail.ru>
2020-07-10 16:29:40 +0300

    Added log user in and debug logic

--- login.php
+++ login.php
@@ -9,7 +9,15 @@ AuthClass::checkLoginIsset();
 
 if (AuthClass::checkLoginIsset()){
     $loginCleanedData = AuthClass::cleanLoginData();
-    AuthClass::loginUser($loginCleanedData['login'], $loginCleanedData['password']);
+    $loginStatus = AuthClass::loginUser($loginCleanedData['login'], $loginCleanedData['password']);
+    if ($loginStatus['is_logged_in']==true){
+        print_r($_COOKIE);
+        echo "-------------------- <br>";
+        print_r($_SESSION);
+    }
+    else {
+        print_r($loginStatus['errors']);
+    }
 }
 else {
     print_r($_POST);
