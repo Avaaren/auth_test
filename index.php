bf79e63c6d76f2a2016f15291e4b6c4330889d82
Avaaren <avaaren@mail.ru>
2020-07-10 18:33:05 +0300

    Included all required content to index page

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-require_once './config/conf.php';
+require_once './auth_handling/config/conf.php';
 ?>
 
 <!DOCTYPE html>
@@ -8,10 +8,20 @@ require_once './config/conf.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main page</title>
-    <link rel="stylesheet" href="./static/css/base.css">
+    <link rel="stylesheet" href="static/css/base.css">
 </head>
 <body>
-<?php include './static/page_components/header.php'; ?>
-<?php include './static/page_components/registration_form.php'; ?>
+<?php include './page_components/header.php'; ?>
+<?php if (isset($_SESSION['login_user'])):?>
+    <?php 
+        echo "Hello".$_SESSION['login_user'];
+    ?>
+<?php else:?>
+    <?php include './page_components/registration_form.php' ?>
+    <?php include './page_components/login_form.php' ?>
+<?php endif; ?>
+
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+<script src="static/js/base.js"></script>
 </body>
 </html>
