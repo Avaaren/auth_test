479e97966608d7f2e7c4c69c259535049881fa93
Avaaren <avaaren@mail.ru>
2020-07-11 11:10:22 +0300

    Added registration form ajax sending

--- static/js/base.js
+++ static/js/base.js
@@ -1,13 +1,39 @@
 $(document).ready(function(){
+    // Methods switching forms
     $('#registration-button').on('click', function(event){
-        event.preventDefault;
+        event.preventDefault();
         $('#registration-form').show();
         $('#login-form').hide();
     });
 
     $('#login-button').on('click', function(event){
-        event.preventDefault;
+        event.preventDefault();
         $('#registration-form').hide();
         $('#login-form').show();
+    });
+
+    // Ajax form sending methods
+
+    $('#registration-submit').on('click', function(event){
+        event.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: '/auth_test/auth_handling/registration.php',
+            data: $("#registration-form input").serialize(),
+            success: function(data){
+                console.log(data);
+                var jsonData = JSON.parse(data);
+                console.log(jsonData);
+                if (!jsonData.success && jsonData.errors){
+                    jsonData.errors.forEach((item) => {
+                        alert(item);
+                    });
+                }
+                else {
+                    alert('Успешная регистрация');
+                    window.location.href = '/auth_test/';
+                }
+            }
+        });
     });
 });
