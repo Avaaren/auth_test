f24e87250a4a02bdc979b85c2c5313e89dd0575f
Avaaren <avaaren@mail.ru>
2020-07-11 11:47:44 +0300

    Added required for all fields

--- page_components/login_form.php
+++ page_components/login_form.php
@@ -1,7 +1,7 @@
 <div class="form-block" id="login-form">
 <form method="post" action="auth_handling/login.php" class=auth-form>
-        <input type="text" id="login" name="login" placeholder="login"> <br>
-        <input type="password" id="password" name="password" placeholder="password"> <br>
+        <input type="text" id="login" name="login" placeholder="login" required> <br>
+        <input type="password" id="password" name="password" placeholder="password" required> <br>
     </form>
     <button id="login-submit" class="form-button">Войти</button>
 </div>
--- page_components/registration_form.php
+++ page_components/registration_form.php
@@ -1,10 +1,10 @@
 <div class="form-block" id="registration-form">
     <form method="post" action="registration.php" id="registration-form" class="auth-form">
-        <input type="text" id="login" name="login" placeholder="login"> <br>
-        <input type="email" id="email" name="email" placeholder="email"> <br>
-        <input type="text" id="name" name="name" placeholder="name"> <br>
-        <input type="password" id="password" name="password" placeholder="password"> <br>
-        <input type="password" id="password2" name="password2" placeholder="password2"> <br>
+        <input type="text" id="login" name="login" placeholder="login" required> <br>
+        <input type="email" id="email" name="email" placeholder="email" required> <br>
+        <input type="text" id="name" name="name" placeholder="name" required> <br>
+        <input type="password" id="password" name="password" placeholder="password" required> <br>
+        <input type="password" id="password2" name="password2" placeholder="password2" required> <br>
     </form>
     <button id="registration-submit" class="form-button">Зарегестрироваться</button>
 </div>
