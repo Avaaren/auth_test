e29e3c8a4ccce2c81b8be76d86a1f81119e300fb
Avaaren <avaaren@mail.ru>
2020-07-11 11:08:49 +0300

    Reworked registration logic for ajax handling

--- auth_handling/registration.php
+++ auth_handling/registration.php
@@ -5,26 +5,38 @@ include 'AuthClass.php';
 
 use Lol\AuthClass;
 
+$result = array();
+
+function unsuccessfulResponse($error){
+    if (is_array($error)){
+        $result['errors'] = $error;
+    }
+    else {
+        array_push($result['errors'], $error);
+    }
+    $result['success'] = false;
+    echo json_encode($result);
+}
+
 if (AuthClass::checkRegistrationIsset()){
     $registrationCleanedData = AuthClass::cleanRegistrationData();
     # If errors array has errors -> display them
     if (!empty($registrationCleanedData['errors'])){
-        foreach ($registrationCleanedData['errors'] as $error){
-            echo $error;
-        }
+        unsuccessfulResponse($registrationCleanedData['errors']);
     }
     else {
         if (AuthClass::checkUserUniqueness($registrationCleanedData['login'], $registrationCleanedData['email'])){
             AuthClass::registerUser($registrationCleanedData);
+            echo json_encode(array('success' => true, 'errors' => null));
         }
         else {
-            echo "Такой пользователь уже существует";
+            unsuccessfulResponse("Такой пользователь уже существует");
         }
     }
     
 
 }
 else {
-    print_r($_POST);
+    unsuccessfulResponse("Неверно отправлен запрос");
 }
 ?>
